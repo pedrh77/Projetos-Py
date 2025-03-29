4084704b3daeda538c1836b725f998f86355908b
Pedro Santos <66256107+pedrh77@users.noreply.github.com>
2025-03-29 00:31:22 +0000

    Alterações de projeto

--- main.py
+++ main.py
@@ -1,22 +1,68 @@
 import sqlite3 as DB
+from  Models.Pessoa import Pessoa
 
-try:
+def ConectAndRun(command):
+    try:
     
-    connect = DB.connect("./banco.db")
-    cursor = connect.cursor()
+        connect = DB.connect("./banco.db")
+        cursor = connect.cursor()
+        
+       
+        response = cursor.execute(command)
+        
+        connect.commit()
+        
+        return response
     
-    comand = """
+    except  DB.DatabaseError as Err:
+        print("Erro banco", Err)
     
-    """;
-    cursor.execute(comand)
+    finally:
+        if connect:
+            cursor.close()
+            connect.close()
     
-    connect.commit()
     
-except  DB.DatabaseError as Err:
-    print("Erro banco", Err)
+
+
+def CadastraAluno():
+    print("==== Cadastro de Alunos ====")
+    while opc_aluno != "0":
+        matricula_al = input("Digite a matricula do Aluno")
+        nome_al = input("Qual o nome do Aluno")
+        email_al = input("Qual o email")
+        
+        command = "Select matricula from Aluno where matricula ={matricula_al}"
+        result = ConectAndRun(command)
+        
+        
+        
+        
+        
+        
     
-finally:
-    if connect:
-        cursor.close()
-        connect.close()
     
+    
+    
+def CadastraMateria():
+def CadastraNota():
+
+def main():
+    while opc != '0':
+        print ("O que deseja fazer hoje?")
+        print(" 0 - Sair")
+        print(" 1 - Cadastrar Aluno")
+        print(" 2 - Cadastrar Materias")
+        print(" 3 -  Cadastrar Nota")
+        
+        opc = input("O que deseja fazer hoje?")
+        match opc:
+            case"1":
+                CadastraAluno()
+            case "2":
+                CadastraMateria()
+            case "3":
+                CadastraNota()
+            case _:
+                print("Digite uma opção Valida")
+    print("Saindo do programa")
